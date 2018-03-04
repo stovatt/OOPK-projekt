d31b24698279da6695121b083f3aeeea56bcbe57
tpilaae <lukas16a@gmail.com>
2018-03-04 15:52:42 +0100

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projektuppgift;
+
+import java.io.IOException;
+import java.util.Observable;
+import java.util.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lukasgu
+ */
+public class Server extends Observable implements Runnable{
+    
+    private ServerSocket myServerSocket;
+    
+    public Server(int port){
+        try {
+            myServerSocket = new ServerSocket(port);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void run(){
+        Socket proposer = null;
+        int start = 0;
+        int end = 0;
+        while(true){
+            try {
+                proposer = myServerSocket.accept();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            start = (int) System.currentTimeMillis();
+            while((end - start) < 30000){
+                
+
+
+                end = (int) System.currentTimeMillis();
+            }
+
+
+
+            proposer = null;
+                    
+                    
+        }
+        
+        
+    }
+    
+    
+}
