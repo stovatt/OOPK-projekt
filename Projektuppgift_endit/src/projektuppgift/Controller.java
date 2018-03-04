466a41bd125f996fae8daa659cedd82d3a0d4d97
stovatt <sarah.tovatt@gmail.com>
2018-03-04 13:13:41 +0100

    Update Controller.java

--- Projektuppgift_endit/src/projektuppgift/Controller.java
+++ Projektuppgift_endit/src/projektuppgift/Controller.java
@@ -20,6 +20,11 @@ public class Controller extends Observable implements Observer, ActionListener{
     private View myView;
     private Model myModel;
     
+    public Controller(View inView, Model inModel){
+        myView = inView;
+        myModel = inModel;
+    }
+    
     public String[] getMembers(ChatModel chat){
     
         return null;
