e8900018bc7d5d084b318a227bf72c9050e319e3
stovatt <sarah.tovatt@gmail.com>
2018-03-05 15:14:57 +0100

    Update View.java

--- Projektuppgift/src/projektuppgift/View.java
+++ Projektuppgift/src/projektuppgift/View.java
@@ -35,7 +35,7 @@ public class View extends Observable implements Observer, ActionListener{
     private ArrayList<ChatModel> chatList;
     private JTabbedPane historyPanels;
     private ArrayList<JTextPane> chatHistorys;
-    private JTextArea MsgBox;
+    private JTextArea msgBox;
     private JButton NewChatBtn;
     private JButton PersonalSettingsBtn;
     private JButton SendMsgBtn;
@@ -43,10 +43,12 @@ public class View extends Observable implements Observer, ActionListener{
     private JButton SendFileBtn;
     private JButton connectToChatBtn;
     private JButton kickButton;
+    private JButton closeBtn;
     private JFrame TheWindow;
     
     public View(){
-        new StartWindow();
+        StartWindow s = new StartWindow();
+        s.addObserver(this);
     }
     
     private void draw(){
@@ -57,7 +59,7 @@ public class View extends Observable implements Observer, ActionListener{
         // Chat history and chat control buttons
         
         historyPanels = new JTabbedPane();
-        historyPanels.setPreferredSize(new Dimension(400, 200));
+        historyPanels.setPreferredSize(new Dimension(600, 500));
         historyPanels.addChangeListener(changeListener);
         
         chatList = new ArrayList<>();
@@ -67,37 +69,42 @@ public class View extends Observable implements Observer, ActionListener{
         connectToChatBtn = new JButton("Connect to server");
         PersonalSettingsBtn = new JButton("Open settings");
         kickButton = new JButton("Kick from chat");
-        SendFileBtn = new JButton("Send File");
+        closeBtn = new JButton("Close");
+        
+        closeBtn.setBackground(Color.RED);
         
         JPanel controlButtonPanel = new JPanel();
         controlButtonPanel.setLayout(new GridLayout(0,1));
         
         controlButtonPanel.add(NewChatBtn);
+        controlButtonPanel.add(connectToChatBtn);
         controlButtonPanel.add(PersonalSettingsBtn);
         controlButtonPanel.add(kickButton);
-        controlButtonPanel.add(SendFileBtn);
-        controlButtonPanel.add(connectToChatBtn);
+        controlButtonPanel.add(closeBtn);
         
         JSplitPane controlPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
                            historyPanels, controlButtonPanel);
         
         //Buttons and textfields for sending messages
         
-        MsgBox = new JTextArea("", 8, 35);
-        MsgBox.setEditable(true);
+        msgBox = new JTextArea("", 8, 40);
+        msgBox.setEditable(true);
         JPanel MsgPanel = new JPanel();
-        MsgPanel.add(MsgBox);
+        MsgPanel.add(msgBox);
         JTabbedPane msgTabbedPanel = new JTabbedPane();
         msgTabbedPanel.addTab("Write here", null, MsgPanel,
                                     "This is a tab, but it looks ok.");
         
         SendMsgBtn = new JButton("Send Message");
+        SendMsgBtn.setBackground(Color.GREEN);
         SendAndEncryptBtn = new JButton("Send+Encrypt Message");
+        SendFileBtn = new JButton("Send File");
         JPanel MsgButtonPanel = new JPanel();
         
         MsgButtonPanel.setLayout(new GridLayout(0,1));
         MsgButtonPanel.add(SendMsgBtn);
         MsgButtonPanel.add(SendAndEncryptBtn);
+        MsgButtonPanel.add(SendFileBtn);
         
         JSplitPane msgSplitPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
                            msgTabbedPanel, MsgButtonPanel);
@@ -111,6 +118,7 @@ public class View extends Observable implements Observer, ActionListener{
         PersonalSettingsBtn.addActionListener(this);
         kickButton.addActionListener(this);
         connectToChatBtn.addActionListener(this);
+        closeBtn.addActionListener(this);
         
         // Put together, pack and set visible
         
@@ -149,19 +157,22 @@ public class View extends Observable implements Observer, ActionListener{
     }
     
     public void openSettings(){
-        SettingsWindow setWindow = new SettingsWindow();
+        User me = theModel.getMe();
+        SettingsWindow setWindow = new SettingsWindow(me.getName(), me.getColor());
         setWindow.addObserver(this);
     }
     
     public void sendMsg(int[] ind){
-        String msgText = MsgBox.getText();
+        String msgText = msgBox.getText();
         
-        Color color = activeChat.getMe().getColor();
-        String name = activeChat.getMe().getName();    
+        Color color = theModel.getMe().getColor();
+        String name = theModel.getMe().getName();    
             
         Message message = new Message(name, color, msgText, ind, false, false);
         activeChat.sendMsg(message, theModel.getMe());
-        System.out.println(message);
+//        System.out.println(message);
+        
+        msgBox.setText("");
     }
     
     public void askForEncryptIndex(String msgText){
@@ -189,6 +200,45 @@ public class View extends Observable implements Observer, ActionListener{
     
     public void kick(){
         
+    }
+    
+    public void leave(){
+        System.out.println("leave början");
+        if(activeChat != null){
+            System.out.println("leave aC != null");
+            String n = theModel.getMe().getName();
+            Color c = theModel.getMe().getColor();
+            int[] i = new int[] {};
+            Message leaveM = new Message(n, c, "Hejdå", i, true, false);
+            activeChat.sendMsg(leaveM, theModel.getMe());
+
+            Message tempM = new Message("Server", Color.BLACK,
+                    "You have left this chat", i, false, false);
+            updateChatHistory(tempM, activeChat);
+            
+            int chatIndex = chatList.indexOf(activeChat);
+            chatHistorys.remove(chatIndex);
+            chatList.remove(chatIndex);
+//            historyPanels.remove(chatIndex);
+            
+        }
+        
+        if(chatList.size() > 0){
+            activeChat = chatList.get(0);
+        }
+        else{
+            activeChat = null;
+        }
+    }
+    
+    public int close(){
+        System.out.println("close, innan leave");
+        leave();
+        System.out.println("close, efter leave");
+        TheWindow.dispose();
+        System.exit(0);
+        
+        return 0;
     }
     
     public void updateChatHistory(Message newMessage, ChatModel chat){
@@ -220,7 +270,15 @@ public class View extends Observable implements Observer, ActionListener{
     public void update(Observable o, Object arg){
         if(o instanceof ChatModel){
             if(arg instanceof Message){
-                updateChatHistory((Message)arg, (ChatModel)o);
+                Message message = (Message)arg;
+                if(message.isDisconnect()){
+                    message.setText(message.getName() + " har loggat ut");
+                    message.setName("Server");
+                }
+                else if(false){
+                    
+                }
+                updateChatHistory(message, (ChatModel)o);
             }
         }
         if(o == theModel){
@@ -249,8 +307,9 @@ public class View extends Observable implements Observer, ActionListener{
             System.out.println(ind);
         }
         if(o instanceof StartWindow){
+            System.out.println("hej");
             Object[] newArg = (Object[])arg;
-            int port = (int)newArg[1];
+            int port = Integer.parseInt((String)newArg[1]);
             User me = (User)newArg[0];
             theModel = new Model(port);
             theModel.addObserver(this);
@@ -286,7 +345,7 @@ public class View extends Observable implements Observer, ActionListener{
             sendMsg(ind);
         }
         else if(e.getSource() == SendAndEncryptBtn){
-            String msgText = MsgBox.getText();
+            String msgText = msgBox.getText();
             askForEncryptIndex(msgText);
         }
         else if(e.getSource() == SendFileBtn){
@@ -307,12 +366,15 @@ public class View extends Observable implements Observer, ActionListener{
                 kick();
             }
             else{
-                
+                leave();
             }
 
         }
         else if(e.getSource() == connectToChatBtn){
             connectToServer();
+        }
+        else if(e.getSource() == closeBtn){
+            close();
         }
     }
 }
